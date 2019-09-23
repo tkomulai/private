afdbc7555da409771a66041e9c054df0ebe2b310
Teemu Komulainen <teemu.komulainen@student.hamk.fi>
2019-09-23 13:20:45 +0300

    testing

--- Python/helloworld.py
+++ Python/helloworld.py
@@ -1,1 +1,3 @@
-print('Hello\nHello again')
+first_name = input('Please enter your first name: ')
+last_name = input('Please enter your last name: ')
+print('Hello ' + first_name.capitalize() + ' ' + last_name.capitalize())
